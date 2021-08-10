a41bdb2dd6d908e8ecc041380ba655e33999f2d6
Mickaelle Pazuelo <mickaelle.pazuelo@gmail.com>
2021-08-10 18:14:10 +0200

    it2

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,10 +6,57 @@ export class Contact extends Component {
     state = {
         contacts: contacts.slice(0,5)
 
+    };
+
+    addRandomContact = () => {
+      const randomContact = 
+      contacts[Math.floor(Math.random() * contacts.length)]
+      const copy = [...this.state.contacts];
+
+      copy.push(randomContact);
+      
+      // this.state.contacts.push(randomContact);
+      this.setState({
+        contacts: copy,
+      });
+      
+    };
+
+    sortByName = () => {
+      const copy = [...this.state.contacts];
+      copy.sort(function (a, b) {
+        var nameA = a.name.toUpperCase();
+        var nameB = b.name.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if(nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+
+      
+      this.setState({
+        contacts: copy,
+      });
+
+    };
+
+    sortByPopularity = () => {
+      this.state.contacts.sort (function (a, b) {
+        popularityA = a.popularity
+        popularityB = b.popularity
+        return a.popularity-b.popularity 
+
     }
+
   render() {
     return (
-     
+     <div> 
+       <button onClick={this.addRandomContact}>Create</button>
+       <button onClick={this.sortByName}>Sort by Name</button>
+       <button onClick={this.sortByPopularity}>Sort by Pop</button>
       <div>
         <table className="Contact">
         <tr>
@@ -20,9 +67,14 @@ export class Contact extends Component {
          {this.state.contacts.map((contact)=>{
 
 return(<div>
-            <tr>
+            <tr key={contact.id}>
              <td>
-               <img src={contact.pictureUrl} alt="" />
+               <img style={{
+                 height: 80,
+                 width: 80,
+               }}
+                src={contact.pictureUrl} 
+                alt="" />
              </td>
              <td>{contact.name}</td>
              <td>{contact.popularity}</td>
@@ -30,6 +82,7 @@ return(<div>
            </div>)
          })}
         </table>
+         </div>
          </div>
        
     );
